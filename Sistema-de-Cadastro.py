ef10c6b3ba2d5f67e4e2d5e7a45c70f23b68c254
YaraAnalysis <114318856+YaraAnalysis@users.noreply.github.com>
2023-02-15 22:13:13 -0300

    Update Sistema-de-Cadastro.py

--- Sistema-de-Cadastro.py
+++ Sistema-de-Cadastro.py
@@ -3,8 +3,8 @@ from cadastro import cadastro
 print('Sistema de cadastro')
 alvo = input('Deseja fazer um cadastro de aluno, professor ou monitor? ')
 
-# chamar a funão de cadastro(alvo)
 
+# chamar a funão de cadastro(alvo)
 cadastrar = 'S'
 while cadastrar.upper() == 'S':
     cadastro(alvo)
